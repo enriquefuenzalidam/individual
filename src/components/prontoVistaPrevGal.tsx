876b6eec8773a2cf816e169c7cb79a7aa9c38097
enriquefuenzalidam <enrique.fuenzalida@yahoo.cl>
2025-02-22 22:13:47 -0300

    Update prontoVistaPrevGal.tsx

--- src/components/prontoVistaPrevGal.tsx
+++ src/components/prontoVistaPrevGal.tsx
@@ -101,10 +101,10 @@ const ProntoVistaPrevGal: React.FC<ProntoVistaPrevGalProps> = ({ imagenesLista, 
                     willChange: "transform, opacity", transition: "all 700ms ease-in-out", position: "absolute", height: "calc(100% - 4rem)", aspectRatio: "1 / 1", borderRadius: "0.125rem", overflow: "hidden", display: "block", opacity: (isSideImage || isCurrent) ? "1" : "0", zIndex: isCurrent ? "50" : (isBefore1 || isAfter1) ? "40" : (isBefore2 || isAfter2) ? "30" : "10",
 
                     boxShadow: isSideImage ? "0 10px 15px -3px rgba(0, 0, 0, 0.4), 0 4px 6px -2px rgba(0, 0, 0, 0.4)" : isCurrent ? "0 10px 15px -3px rgba(0, 0, 0, 0.6), 0 4px 6px -2px rgba(0, 0, 0, 0.6)" : "none",
-                    top: "1.25rem", left: isCurrent ? "50%" : isBefore1 ? "25%" : isAfter1 ? "75%" : isBefore2 ? "0%" : "100%",
+                    top: "1.25rem", left: isBefore1 ? "25%" : isAfter1 ? "75%" : isBefore2 ? "0%" : isAfter2 ? "100%" : "50%",
                     transform: `
-                        translateX(${isCurrent ? "-50%" : isBefore1 ? "-25%" : isAfter1 ? "-75%" : isBefore2 ? "0%" : "-100%"})
-                        scale(${isCurrent ? 1.1 : (isBefore1 || isAfter1) ? 1.05 : (isBefore2 || isAfter2) ? 0.95 : 0.25})`,
+                        translateX(${ isBefore1 ? "-25%" : isAfter1 ? "-75%" : isBefore2 ? "0%" : isAfter2 ? "-100%" : "-50%" })
+                        scale(${isCurrent ? 1.1 : (isBefore1 || isAfter1) ? 1.05 : (isBefore2 || isAfter2) ? 0.95 : 0.01})`,
 
                 }
                 const sobreCapaStyle = {
@@ -122,7 +122,7 @@ const ProntoVistaPrevGal: React.FC<ProntoVistaPrevGalProps> = ({ imagenesLista, 
                         !loadedImages[index] && (React.createElement('div', { style: imageBlockStyleC },
                             React.createElement('svg', { xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 640 512", fill: 'currentColor' },
                                 React.createElement('path', { fill: 'currentColor', d: 'M54.2 202.9C123.2 136.7 216.8 96 320 96s196.8 40.7 265.8 106.9c12.8 12.2 33 11.8 45.2-.9s11.8-33-.9-45.2C549.7 79.5 440.4 32 320 32S90.3 79.5 9.8 156.7C-2.9 169-3.3 189.2 8.9 202s32.5 13.2 45.2 .9zM320 256c56.8 0 108.6 21.1 148.2 56c13.3 11.7 33.5 10.4 45.2-2.8s10.4-33.5-2.8-45.2C459.8 219.2 393 192 320 192s-139.8 27.2-190.5 72c-13.3 11.7-14.5 31.9-2.8 45.2s31.9 14.5 45.2 2.8c39.5-34.9 91.3-56 148.2-56zm64 160a64 64 0 1 0 -128 0 64 64 0 1 0 128 0z' })))),
-                        React.createElement(NextImage, { src: typeof item === "string" ? item : item.src, alt: 'Gallery Image', fill: true, style: { objectFit: 'cover', transition: 'all 700ms ease-in-out', willChange: "opacity", opacity: loadedImages[index] ? 1 : 0 } }),
+                        (isSideImage || isCurrent) && React.createElement(NextImage, { src: typeof item === "string" ? item : item.src, alt: 'Gallery Image', fill: true, style: { objectFit: 'cover', transition: 'all 700ms ease-in-out', willChange: "opacity", opacity: loadedImages[index] ? 1 : 0 } }),
                         React.createElement("div", { style: sobreCapaStyle, onClick: isCurrent || !(isBefore1 || isBefore2 || isAfter1 || isAfter2) ? undefined : () => handleNavClick(index) })
                     )
                 )
