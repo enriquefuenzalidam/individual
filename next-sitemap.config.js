ba816cc894c7abb643b2e84b70e63be15fe62183
enriquefuenzalidam <enrique.fuenzalida@yahoo.cl>
2025-03-09 12:07:25 -0300

    robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,10 @@
 module.exports = {
     siteUrl: 'https://individual.cl',
     generateRobotsTxt: true,
+    robotsTxtOptions: {
+      policies: [
+        { userAgent: '*', allow: '/' },
+        { userAgent: '*', disallow: ['/src/', '/app/', '/assets/', '/components/', '/hooks/'] }
+      ]
+    }
   };
