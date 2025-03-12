28be0ea2d43620b41de5c52bf9ea7b30d9d6c21d
enriquefuenzalidam <enrique.fuenzalida@yahoo.cl>
2025-03-12 18:50:35 -0300

    sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,13 @@ import Footer from "@/components/footer";
 import ProntoVistaArticulo from './prontoVistaArticulo';
 
 const Home: React.FC = () => {
-  return React.createElement('main', { style: { position: 'relative', width: '100%', maxWidth: '64rem', transition: 'all 300ms ease-in-out' } },
+
+  return React.createElement('div', { style: { position: 'relative', width: '100%', maxWidth: '64rem', transition: 'all 300ms ease-in-out' } },
     React.createElement(Header, null),
-    React.createElement(ProntoVistaArticulo, null ),
-    React.createElement(Footer, null)
-   )
+    React.createElement('main', null,
+      React.createElement(ProntoVistaArticulo, null ) ),
+    React.createElement(Footer, null) )
+
 }
 
 export default Home;
