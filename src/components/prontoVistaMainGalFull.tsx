3ffe0d2fe451a8647bdead15e3a378e0ef1555d0
enriquefuenzalidam <enrique.fuenzalida@yahoo.cl>
2025-03-21 06:21:12 -0300

    loadedImages

--- src/components/prontoVistaMainGalFull.tsx
+++ src/components/prontoVistaMainGalFull.tsx
@@ -174,7 +174,7 @@ const ProntoVistaMainGalFull: React.FC<ProntoVistaFullProps> = ({ imagenesLista,
 
                 React.createElement('section', { onClick: resetCountdown, role: "region", "aria-label": "Full-size Image", style: { display: 'block', boxSizing: 'border-box', position: 'absolute', inset: '0', background: 'black', cursor: 'default' }},
                     imagenesLista?.map((item, index) => React.createElement('div', {key: index, style: { display: 'block', boxSizing: 'border-box', position: 'absolute', inset: '0', background: 'transparent', opacity: currentIndex === index ? '1' : '0', transition: 'opacity 0.5s ease-in-out', pointerEvents: currentIndex === index ? 'auto' : 'none' } },
-                        !(loadedImages.every((loaded) => loaded)) && loadingImageFullImage,
+                        !loadedImages && loadingImageFullImage,
                         visibleImages[index] && React.createElement(NextImage, { key: index, onLoad: () => handleImageLoad(index), src: item, alt: 'Gallery Image', style: { position: 'relative', width: '100%', height: '100%', objectFit: 'contain' } } ) ) ) ),
 
                 React.createElement('section', { role: "region", "aria-label": "Image Thumbnails", style: { display: 'block', boxSizing: 'border-box', opacity: mostrarOcultarLista ? 1 : 0, position: 'absolute', bottom: '0', left: '0', width: '100%', height: tnScreen ? '6rem' : smScreen ? '7rem' : mdScreen ? '8rem' : lgScreen ? '9rem' : xlScreen ? '10rem' : '10rem', overflow: 'hidden', maskImage: 'linear-gradient( to right, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 13%, rgba(0,0,0,1) 87%, rgba(0,0,0,0) 100%)', transition: 'all 600ms ease-in-out', pointerEvents: mostrarOcultarLista ? 'auto' : 'none' } },
