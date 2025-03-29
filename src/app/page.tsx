318ceaad978449864a1f8b31826274f3e768d108
enriquefuenzalidam <enrique.fuenzalida@yahoo.cl>
2025-03-29 12:30:15 -0300

    notfound

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ import ProntoVistaArticulo from './prontoVistaArticulo';
 
 const Home: React.FC = () => {
 
-  return React.createElement('div', { style: { position: 'relative', width: '100%', maxWidth: '64rem', transition: 'all 300ms ease-in-out' } },
+  return React.createElement('div', { style: { position: 'relative', boxSizing: 'border-box', display: 'flex', flexDirection: 'column', height:'100%', width: '100%', maxWidth: '64rem', transition: 'all 300ms ease-in-out' } },
     React.createElement(Header, null),
-    React.createElement('main', null,
+    React.createElement('main', { style: { flexGrow: 1 } },
       React.createElement(ProntoVistaArticulo, null ) ),
     React.createElement(Footer, null) )
 
