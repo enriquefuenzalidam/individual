8abb2404691187183fb1e92d93a9aea027daf8db
enriquefuenzalidam <enrique.fuenzalida@yahoo.cl>
2025-03-30 17:40:56 -0300

    contendor

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import Header from "@/components/header";
 import Footer from "@/components/footer";
-import NotFoundContent from './NotFoundContent';
+import NotFoundContent from './notFoundContent';
 
 const NotFound: React.FC = () => {
 
