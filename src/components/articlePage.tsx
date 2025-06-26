17c7e5244a747bb15e42364b22a9f483530a1956
enriquefuenzalidam <enrique.fuenzalida@yahoo.cl>
2025-06-26 03:57:42 -0400

    oitrrrrs

--- src/components/articlePage.tsx
+++ src/components/articlePage.tsx
@@ -14,7 +14,8 @@ const ArticlePage: React.FC<{ children: React.ReactNode }> = ({ children }) => {
         const timeout = setTimeout(() => setPageLoaded(true), 500);
         return () => clearTimeout(timeout); }, [screenReady]);
 
-    return React.createElement('div', { style: { position: 'relative', margin: '0', padding: '0', boxSizing: 'border-box', display: 'block', height:'auto', width: '100%', backgroundColor: 'white', background: 'linear-gradient( 54deg, white 15%, #fffaf4 38%, white)' } },
+    return React.createElement('div', { style: { position: 'relative', margin: '0', padding: '0', boxSizing: 'border-box', display: 'block', height:'auto', width: '100%', /*'linear-gradient( 54deg, white 15%, #fffaf4 38%, white)'*/ } },
+        React.createElement('div', { style: { display: 'block', position: 'absolute', boxSizing: 'border-box',top: '0', left: '0', width: '100%', height: '100%', padding: '0', margin: '0', backgroundColor: 'white', backgroundImage: `url('/bb.jpg')`, backgroundPosition: '50%', backgroundAttachment: 'scroll', backgroundRepeat: 'repeat', opacity: 0.3 } }),
         React.createElement('div', { style: { position: 'relative', margin: '0', padding: '0', boxSizing: 'border-box', display: 'block', maxWidth: '64rem', height:'auto', width: '100%', } },
             React.createElement(Header, null),
             React.createElement('main', { style: { ...nullBlockStyle, padding: dynSzGttr( '5', '4.36', '3.72', '3.08', '2.44', '1.8') + 'rem 0 ' + dynSzGttr('5', '4.36', '3.72', '3.08', '2.44', '1.8') + 'rem 0', margin: 0, transition: 'opacity 500ms ease-in-out', opacity: pageLoaded ? 1 : 0, pointerEvents: pageLoaded ? 'auto' : 'none' } },
